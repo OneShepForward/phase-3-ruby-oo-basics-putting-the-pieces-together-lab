7db9dcfb8ea75ea4ef46bbfd07b39225568efc5b
Nicholas Shephard <shephardn@gmail.com>
2021-12-15 07:19:01 -0600

    commit tree 7c7a0477658d145a033659964f1dfc4ae9862bb3

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,27 @@
+require 'pry'
 
+class Book
+
+    # attr_accessor :title 
+    # attr_accessor :author
+    # attr_accessor :page_count
+    # attr_accessor :genre
+
+    # this way don't work :(
+    attr_accessor :title, :author, :page_count, :genre
+
+    def initialize(title, author = "None", page_count = "Unknown", genre = "None")
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+    
+end
+
+# binding.pry
 
